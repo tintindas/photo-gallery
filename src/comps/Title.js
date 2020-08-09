fd87b6871a835d92eacc737a4b7b53a88da818e9
Upamanyu Das <upamanyu.das.work@gmail.com>
2020-08-09 18:27:48 +0530

    Add login

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,20 +1,27 @@
 import React from "react";
 import LoginForm from "./LoginForm";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const Title = ({ user }) => {
   return (
-    <div className='title'>
-      <div className='nav'>
-        <h1>ArtGram</h1>
-        <LoginForm user={user} />
+    <Router>
+      <div className='title'>
+        <div className='nav'>
+          <h1>ArtGram</h1>
+          <Route
+            path='/login'
+            exact
+            render={(props) => <LoginForm {...props} user={user} />}
+          />
+        </div>
+        <h2>Hi, I'm Ujjaini</h2>
+        <p style={{ marginBottom: !user ? "5vh" : "" }}>
+          I'm an India-based artist and photographer.
+          <br />
+          Welcome to my gallery 😊
+        </p>
       </div>
-      <h2>Hi, I'm Ujjaini</h2>
-      <p style={{ marginBottom: !user ? "5vh" : "" }}>
-        I'm an India-based artist and photographer.
-        <br />
-        Welcome to my gallery 😊
-      </p>
-    </div>
+    </Router>
   );
 };
 
